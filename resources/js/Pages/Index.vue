84893ffbe81e567e31bf2c279b8e90e228c041d2
VenuxAK <shelby.ak68@gmail.com>
2025-10-06 01:04:19 +0000

    Setup Inertia + Vue

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+    <div>
+        <h1 class="text-center font-bold text-purple-500 text-3xl">
+            Index Page
+        </h1>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+        vue(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/js"),
+            "@components": path.resolve(__dirname, "resources/js/Components"),
+            "@pages": path.resolve(__dirname, "resources/js/Pages"),
+            "@layouts": path.resolve(__dirname, "resources/js/Layouts"),
+        },
+    },
+    // resolve: (name) => {
+    //     const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+    //     return pages[`./Pages/${name}.vue`];
+    // },
 });
